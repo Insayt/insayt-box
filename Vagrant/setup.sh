b2d847d1880b1528af4661f2cfc38c190003a702
Insayt <insait.rostov@ya.ru>
2016-03-29 00:04:15 +0300

    fix

--- Vagrant/setup.sh
+++ Vagrant/setup.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
-echo "Installing Git"
-apt-get install git -y
-
-echo "Installing Nginx"
-apt-get install nginx -y
-
-echo "Updating PHP repository"
-apt-get install python-software-properties build-essential -y
+echo "Updating repository"
 add-apt-repository ppa:ondrej/php5 -y
 apt-get update > /dev/null
 
+echo "Install base packages"
+apt-get install curl build-essential python-software-properties
+
+echo "Installing Git"
+apt-get install git -y
+
 echo "Installing PHP"
 apt-get install php5-common php5-dev php5-cli php5-fpm -y
-
-echo "Installing PHP extensions"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y
+apt-get install php5-curl php5-gd php5-mcrypt php5-mysql -y
 
 echo "Preparing MySQL"
 apt-get install debconf-utils -y
-debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+echo "mysql-server mysql-server/root_password password " | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password " | debconf-set-selections
 
 echo "Installing MySQL"
-apt-get install mysql-server -y
+apt-get install mysql-client mysql-server -y
+sudo sed -i "s/bind-address.*=.*/bind-address=0.0.0.0/" /etc/mysql/my.cnf
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS october CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+sudo service mysql restart
+
+echo "Installing Nginx"
+apt-get install nginx -y
 
 echo "Configuring Nginx"
 cp /var/www/Vagrant/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
-service nginx restart
 
 echo "Install Composer"
-apt-get install curl
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+service nginx restart
+service php5-fpm restart
--- Vagrantfile
+++ Vagrantfile
@@ -3,17 +3,17 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
-  config.vm.network "forwarded_port", guest: 80, host: 8080
-  config.vm.network "private_network", ip: "192.168.33.10"
-  config.vm.synced_folder ".", "/var/www", :mount_options => ["dmode=777", "fmode=666"]
 
-  # Optional NFS. Make sure to remove other synced_folder line too
-  #config.vm.synced_folder ".", "/var/www", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
+  config.vm.network "forwarded_port", guest: 80,    host: 8080
+  config.vm.network "forwarded_port", guest: 3306,  host: 3306
+  config.vm.synced_folder ".", "/var/www", :mount_options => ["dmode=777", "fmode=666"]
+  config.vm.network "private_network", ip: "192.168.50.4"
 
   config.vm.hostname = 'insayt.vm'
   config.hostmanager.enabled = true
   config.hostmanager.manage_host = true
 
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
   config.vm.provision :shell, :name => 'setup.sh', :path => 'Vagrant/setup.sh'
   config.vm.provision :hostmanager
 
